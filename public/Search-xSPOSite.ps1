bbc1dd79c36345e9cd2d76b087b35d92f3188c16
ChendrayanV <Chendrayan.Exchange@hotmail.com>
2017-04-12 00:29:02 +0530

    Initial Commit

---
+++ public/Search-xSPOSite.ps1
@@ -0,0 +1,43 @@
+function Search-xSPOSite {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [uri]
+        $Url,
+
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]
+        $QueryText
+    )
+    
+    begin {
+    }
+    
+    process {
+        try {
+            $SPOClientContext = [Microsoft.SharePoint.Client.ClientContext]::new($Url)
+            $SPOClientContext.Credentials = $SPOCredential
+            $KeyWordQuery = [Microsoft.SharePoint.Client.Search.Query.KeywordQuery]::new($SPOClientContext)
+            $KeyWordQuery.QueryText = $QueryText
+            $KeyWordQuery.RowLimit = [int]::MaxValue
+            $SearchExecutor = [Microsoft.SharePoint.Client.Search.Query.SearchExecutor]::new($SPOClientContext)
+            $SearchResults = $SearchExecutor.ExecuteQuery($KeyWordQuery)
+            $SPOClientContext.ExecuteQuery()
+            $SPOClientContext.Dispose()
+            foreach ($SearchResult in $SearchResults.Value.ResultRows) {
+                [pscustomobject]@{
+                    Rank = '{0:N2}' -f ($result.Rank)
+                    Author = $result.DisplayAuthor
+                    Title = $result.Title
+                    Url = $result.LinkingUrl
+                }
+            }
+        }
+        catch {
+            $_.Exception.Message
+        }
+    }
+    
+    end {
+    }
+}
