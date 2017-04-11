b5d21979cd73e2b12485d777e34fbde636da941e
ChendrayanV <Chendrayan.Exchange@hotmail.com>
2017-04-11 23:44:20 +0530

    Initial Commit

---
+++ workinprogress/Search-xSPOSite.ps1
@@ -0,0 +1,21 @@
+function Search-xSPOSite {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [uri]
+        $Url,
+
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [string]
+        $KeyWord
+    )
+    
+    begin {
+    }
+    
+    process {
+    }
+    
+    end {
+    }
+}
