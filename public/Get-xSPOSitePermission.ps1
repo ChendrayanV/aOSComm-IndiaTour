b30c47120fbb45c9ba69743d7cca647a12178cdf
ChendrayanV <Chendrayan.Exchange@hotmail.com>
2017-04-11 12:45:45 +0530

    Initial Commit

---
+++ public/Get-xSPOSitePermission.ps1
@@ -0,0 +1,30 @@
+function Get-xSPOSitePermission {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        $Url
+    )
+    
+    begin {
+    }
+    
+    process {
+        try {
+            $SPOClientContext = [Microsoft.SharePoint.Client.ClientContext]::new($Url)
+            $SPOClientContext.Credentials = $SPOCredential
+            $RoleDefinitions = $SPOClientContext.Web.RoleDefinitions
+            $SPOClientContext.Load($RoleDefinitions)
+            $SPOClientContext.ExecuteQuery()
+            $SPOClientContext.Dispose()
+            foreach($RoleDefinition in $RoleDefinitions) {
+                $RoleDefinition
+            }
+        }
+        catch {
+            $_.Exception.Message
+        }
+    }
+    
+    end {
+    }
+}
