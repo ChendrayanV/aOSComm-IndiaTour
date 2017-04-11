1800ccca6e28120fe0e9e09bd3f255b473d96f4a
ChendrayanV <Chendrayan.Exchange@hotmail.com>
2017-04-12 01:35:02 +0530

    Initial Commit

--- public/Get-xSPONavigationNode.ps1
+++ public/Get-xSPONavigationNode.ps1
@@ -1,5 +1,6 @@
 function Get-xSPONavigationNode {
     [CmdletBinding()]
+    [outputtype('Microsoft.SharePoint.Client.NavigationNode')]
     param (
         [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         $Url,
@@ -13,27 +14,32 @@ function Get-xSPONavigationNode {
     }
     
     process {
-        $SPOClientContext = [Microsoft.SharePoint.Client.ClientContext]::new($Url)
-        $SPOClientContext.Credentials = $SPOCredential
-        switch ($NavigationType) {
-            "GlobalNavigation" {
-                $GlobalNavigationNodes = $SPOClientContext.Web.Navigation.TopNavigationBar
-                $SPOClientContext.Load($GlobalNavigationNodes)
-                $SPOClientContext.ExecuteQuery()
-                $SPOClientContext.Dispose()
-                foreach ($GlobalNavigationNode in $GlobalNavigationNodes) {
-                    $GlobalNavigationNode
+        try {
+            $SPOClientContext = [Microsoft.SharePoint.Client.ClientContext]::new($Url)
+            $SPOClientContext.Credentials = $SPOCredential
+            switch ($NavigationType) {
+                "GlobalNavigation" {
+                    $GlobalNavigationNodes = $SPOClientContext.Web.Navigation.TopNavigationBar
+                    $SPOClientContext.Load($GlobalNavigationNodes)
+                    $SPOClientContext.ExecuteQuery()
+                    $SPOClientContext.Dispose()
+                    foreach ($GlobalNavigationNode in $GlobalNavigationNodes) {
+                        $GlobalNavigationNode | Select-Object -Property ([Microsoft.SharePoint.Client.NavigationNode].GetProperties().Where( {$_.Propertytype -notlike "*Collection*"})).Name
+                    }
+                }
+                "QuickLaunch" {
+                    $QuickLaunchNodes = $SPOClientContext.Web.Navigation.QuickLaunch
+                    $SPOClientContext.Load($QuickLaunchNodes)
+                    $SPOClientContext.ExecuteQuery()
+                    $SPOClientContext.Dispose()
+                    foreach ($QuickLaunchNode in $QuickLaunchNodes) {
+                        $QuickLaunchNode | Select-Object -Property ([Microsoft.SharePoint.Client.NavigationNode].GetProperties().Where( {$_.Propertytype -notlike "*Collection*"})).Name
+                    }
                 }
             }
-            "QuickLaunch" {
-                $QuickLaunchNodes = $SPOClientContext.Web.Navigation.QuickLaunch
-                $SPOClientContext.Load($QuickLaunchNodes)
-                $SPOClientContext.ExecuteQuery()
-                $SPOClientContext.Dispose()
-                foreach ($QuickLaunchNode in $QuickLaunchNodes) {
-                    $QuickLaunchNode
-                }
-            }
+        }
+        catch {
+            $_.Exception.Message
         }
     }
     
